52e8fc3da86eda46ed9f8b58bba0929df1688399
DevilSamdev <92378261+DevilSamdev@users.noreply.github.com>
2021-10-19 15:46:11 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -1,78 +1,68 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-// MongoDB Model
-const User = require('../models/User');
 const Post = require('../models/Post');
 
-// VALIDATION Import
-const { registerValidation, loginValidation } = require('../validation');
-
-// Register User
-router.post('/register', async (req, res) => {
-
-	// Validate User
-	const { error } = registerValidation(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
-
-	// Check if User already in database
-	const emailExist = await User.findOne({ email: req.body.email });
-	if(emailExist) return res.status(400).send('Email already exists');
-
-	// Hash Passwords
-	const salt = await bcrypt.genSalt(10);
-	const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-	// Validated And Create User
-	const user = new User({ 
-		name: req.body.name,
-		email: req.body.email,
-		password: hashedPassword
-	});
+// Create A post for specific user - Create
+router.post('/:uid', async (req, res) => {
+	const post = new Post({
+		userID: req.params.uid,
+		title: req.body.title,
+		description: req.body.description
+	})
 
 	try{
-		const savedUser = await user.save();
-		res.json({ name: user.name, email: user.email });
+		const savedPost = await post.save();
+		res.json(savedPost)
 	} catch (err) {
-		res.json({ message: err });
+		res.json({ message: err })
 	}
 });
 
-// Login User
-router.post('/login', async(req, res) => {
-	const { error } = loginValidation(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
-
-	// Check if Email Exists
-	const user = await User.findOne({ email: req.body.email });
-	if(!user) return res.status(400).send('Email Does Not Exist');
-
-	const validPass = await bcrypt.compare(req.body.password, user.password)
-	if(!validPass) return res.status(400).send('Invalid Password');
-
-	// Create & Assign Token 
-	const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-	res.header('auth-token', token).send(token);
-});
-
-// Delete user 
-router.delete('/:uid/only', async(req, res) => {
+// Get All Posts for specific user - Read
+router.get('/:uid', async(req, res) => {
 	try {
-		const removedUser = await User.remove({ _id: req.params.uid });
-		res.json(removedUser);
+		const posts = await Post.find({ userID: { $eq: req.params.uid } });
+		console.log(posts)
+		res.json(posts);
 	} catch(err) {
 		res.json({ message: err });
 	}
 });
 
-router.delete('/:uid/all', async(req, res) => {
+// Get Specific Post of user - Read
+router.get('/:uid/:postId', async(req, res) => {
 	try {
-		const removedUser = await User.remove({ _id: req.params.uid, get: req.body });
-		const removedPosts = await Post.deleteMany({ userID: req.params.uid });
-		res.json(removedUser);
-		res.json(removedPosts);
+		const post = await Post.findById(req.params.postId);
+		if (post.userID === req.params.uid) {
+			res.json(post);
+		} else {
+			res.json({ message: "Incorrect User" });
+		}
+	} catch(err) {
+		res.json({ message: err });
+	}
+});
+
+// Update Specific Post of a user - Update
+router.patch('/:uid/:postId', async (req, res) => {
+	try {
+		const updatedPost = await Post.updateOne({ _id: req.params.postId, userID: { $eq: req.params.uid } }, {
+			$set: {
+				title: req.body.title
+			}
+		});
+		res.json(updatedPost);
+	} catch(err) {
+		res.json({ message: err });
+	}
+});
+
+// Delete Specific Post of a user - Delete
+router.delete('/:uid/:postId', async(req, res) => {
+	try {
+		const removedPost = await Post.remove({ _id: req.params.postId, userID: { $eq: req.params.uid } });
+		res.json(removedPost);
 	} catch(err) {
 		res.json({ message: err });
 	}
